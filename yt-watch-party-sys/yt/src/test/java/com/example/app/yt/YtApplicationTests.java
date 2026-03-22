1f42682e48f32559431078326f1853867f3c550e
FAHAD AZEEZ <147379218+fahadazeez1@users.noreply.github.com>
2026-03-22 12:25:37 +0530

    Add files via upload

---
+++ yt-watch-party-sys/yt/src/test/java/com/example/app/yt/YtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.app.yt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
